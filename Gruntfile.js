fe472c9bf3faa314ef265924e3065c7de07cd1b8
Varun Agrawal <Varun@VarunAgw.com>
2016-07-29 03:56:06 +0530

    Complete all tasks on file changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -97,7 +97,7 @@ module.exports = function (grunt) {
         watch: {
             scripts: {
                 files: ['source/**/*'],
-                tasks: ['default']
+                tasks: ['all']
             },
             options: {
                 spawn: false
