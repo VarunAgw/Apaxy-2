b949b172c4074c8de2a3e84947c4c8213b79e4e0
Varun Agrawal <Varun@VarunAgw.com>
2017-07-03 22:39:50 +0530

    Cleaning project

--- source/50-initialize_extension.js
+++
@@ -1,7 +0,0 @@
-var apaxy2 = {
-    baseURL: "chrome-extension://" + chrome.i18n.getMessage("@@extension_id") + "/view/"
-};
-
-jQuery.each(resources, function (key) {
-    resources[key] = resources[key].replaceAll("%baseurl%", apaxy2.baseURL);
-});
