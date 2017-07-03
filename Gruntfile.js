17ac715210b1d45922a87ebf338232ef61ea3f8f
Varun Agrawal <Varun@VarunAgw.com>
2017-07-04 03:02:56 +0530

    Restructuring project

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,115 +1,151 @@
 module.exports = function (grunt) {
-    var utils = {
-        read_file: function (path, base64) {
-            if (true === base64) {
-                var content = grunt.file.read(path, {encoding: null});
-                content = new Buffer(content).toString('base64');
-            } else {
-                var content = grunt.file.read(path);
-            }
-            return content;
-        },
-        write_file: function (path, content) {
-            grunt.file.write(path, content);
-        },
-        clone_json: function (json) {
-            return JSON.parse(JSON.stringify(json));
-        },
-        json_to_js: function (var_name, json) {
-            return "var " + var_name + " = " + JSON.stringify(json, null, 2);
-        }
-    };
 
-    grunt.registerTask('start', function () {
-        var resources = {};
+  /*
+   * Utility functions
+   */
+  var utils = {
 
-        resources['base.htm'] = utils.read_file('source/view/base.htm');
-        resources['style.css'] = utils.read_file('source/view/style.css');
-        resources['style_apaxy.css'] = utils.read_file('source/view/style_apaxy.css');
+    read_file: function (path, base64) {
+      var options = base64 ? {encoding: 'base64'} : {};
+      return grunt.file.read(path, options);
+    },
 
-        var resources_extension = utils.clone_json(resources);
-        var resources_plugin = utils.clone_json(resources);
+    write_file: function (path, content) {
+      grunt.file.write(path, content);
+    },
 
-        resources_extension['style.css'] = resources_extension['style.css']
-                .replace('%open-sans-v13-latin-regular.woff2%', '%baseurl%fonts/open-sans-v13-latin-regular.woff2')
-                .replace('%open-sans-v13-latin-regular.woff%', '%baseurl%fonts/open-sans-v13-latin-regular.woff');
+    clone_json: function (json) {
+      return JSON.parse(JSON.stringify(json));
+    },
 
-        resources_plugin['style.css'] = resources_plugin['style.css']
-                .replace('%open-sans-v13-latin-regular.woff2%', 'data:font/woff2;base64,' + utils.read_file('source/fonts/open-sans-v13-latin-regular.woff2', true))
-                .replace('%open-sans-v13-latin-regular.woff%', 'data:application/x-font-woff;base64,' + utils.read_file('source/fonts/open-sans-v13-latin-regular.woff', true));
+    json_to_js: function (var_name, json) {
+      return "var " + var_name + " = " + JSON.stringify(json, null, 2);
+    }
+  };
 
-        grunt.file.write('source/.tmp/30-resources_extension.js', utils.json_to_js('resources', resources_extension));
-        grunt.file.write('source/.tmp/30-resources_plugin.js', utils.json_to_js('resources', resources_plugin));
-    });
+  /*
+   * Process function
+   */
+  grunt.registerTask('init', function (task) {
+    var resources = {};
+
+    resources['base.htm'] = utils.read_file('source/view/base.htm');
+    resources['style.css'] = utils.read_file('source/view/style.css');
+    resources['style_apaxy.css'] = utils.read_file('source/view/style_apaxy.css');
+
+    if (undefined === task || 'extension' === task) {
+      var resources_extension = utils.clone_json(resources);
+
+      resources_extension['style.css'] = resources_extension['style.css']
+              .replace('%open-sans-v13-latin-regular.woff2%', '%baseurl%fonts/open-sans-v13-latin-regular.woff2')
+              .replace('%open-sans-v13-latin-regular.woff%', '%baseurl%fonts/open-sans-v13-latin-regular.woff');
+
+      grunt.file.write('source/.tmp/30-resources_extension.js', utils.json_to_js('resources', resources_extension));
+    }
+
+    if (undefined === task || 'plugin' === task) {
+      var resources_plugin = utils.clone_json(resources);
+
+      resources_plugin['style.css'] = resources_plugin['style.css']
+              .replace('%open-sans-v13-latin-regular.woff2%', 'data:font/woff2;base64,' + utils.read_file('source/fonts/open-sans-v13-latin-regular.woff2', true))
+              .replace('%open-sans-v13-latin-regular.woff%', 'data:application/x-font-woff;base64,' + utils.read_file('source/fonts/open-sans-v13-latin-regular.woff', true));
+
+      grunt.file.write('source/.tmp/30-resources_plugin.js', utils.json_to_js('resources', resources_plugin));
+    }
+  });
 
 
-    grunt.initConfig({
-        concat: {
-            options: {
-                banner: '// Apaxy 2 Server plugin\n\n'
-            },
-            extension: {
-                src: [
-                    'source/00-jquery-2.1.4.min.js',
-                    'source/10-keycode.js',
-                    'source/20-file_types.js',
-                    'source/.tmp/30-resources_extension.js',
-                    'source/40-utils.js',
-                    'source/50-initialize_extension.js',
-                    'source/60-main.js'
-                ],
-                dest: 'extension/content_script.js'
-            },
-            plugin: {
-                src: [
-                    'source/00-jquery-2.1.4.min.js',
-                    'source/10-keycode.js',
-                    'source/20-file_types.js',
-                    'source/.tmp/30-resources_plugin.js',
-                    'source/40-utils.js',
-                    'source/50-initialize_plugin.js',
-                    'source/60-main.js'
-                ],
-                dest: 'plugin/apaxy_2.js'
-            }
-        },
-        uglify: {
-            options: {
-                banner: '// Apaxy 2 Server plugin: (Minified)\n\n'
-            },
-            plugin: {
-                src: 'plugin/apaxy_2.js',
-                dest: 'plugin/apaxy_2.min.js'
-            }
-        },
-        sync: {
-            main: {
-                files: [
-                    {cwd: 'source/icons/', src: '*', dest: 'extension/view/icons/'},
-                    {cwd: 'source/fonts/', src: '*', dest: 'extension/view/fonts/'},
-                    {cwd: 'source/icons', src: '*', dest: 'plugin/icons/'}
-                ],
-                failOnError: true,
-                updateAndDelete: true,
-                compareUsing: "md5"
-            }
-        },
-        watch: {
-            scripts: {
-                files: ['source/**/*'],
-                tasks: ['all']
-            },
-            options: {
-                spawn: false
-            }
-        }
-    });
+  grunt.initConfig({
+    concat: {
+      options: {
+        banner: '// Apaxy 2 Server plugin\n\n'
+      },
+      extension: {
+        src: [
+          'source/00-jquery-2.1.4.min.js',
+          'source/10-keycode.js',
+          'source/20-file_types.js',
+          'source/.tmp/30-resources_extension.js',
+          'source/40-utils.js',
+          'source/50-initialize_extension.js',
+          'source/60-main.js'
+        ],
+        dest: 'builds/development/chrome_extension/content_script.js'
+      },
+      plugin: {
+        src: [
+          'source/00-jquery-2.1.4.min.js',
+          'source/10-keycode.js',
+          'source/20-file_types.js',
+          'source/.tmp/30-resources_plugin.js',
+          'source/40-utils.js',
+          'source/50-initialize_plugin.js',
+          'source/60-main.js'
+        ],
+        dest: 'builds/development/apache_plugin/apaxy_2.js'
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-sync');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    sync: {
+      options: {
+        failOnError: true,
+        updateAndDelete: true,
+        compareUsing: "md5",
+        verbose: true,
+      },
+      extension: {
+        files: [
+          {cwd: 'source/chrome_extension/', src: '**', dest: 'builds/development/chrome_extension/'},
+          {cwd: 'source/icons/', src: '**', dest: 'builds/development/chrome_extension/view/icons/'},
+          {cwd: 'source/fonts/', src: '**', dest: 'builds/development/chrome_extension/view/fonts/'},
+        ],
+        ignoreInDest: "content_script.js",
+      },
+      plugin: {
+        files: [
+          {cwd: 'source/icons', src: '**', dest: 'builds/development/apache_plugin/icons/'}
+        ],
+        ignoreInDest: "apache_2.min.js",
+      },
+      build: {
+        files: [
+          {cwd: 'builds/development/chrome_extension/', src: '**', dest: 'builds/production/chrome_extension/'},
+          {cwd: 'builds/development/apache_plugin/', src: '**', dest: 'builds/production/apache_plugin/'},
+        ],
+      }
+    },
 
-    grunt.registerTask('all', ['start', 'concat', 'uglify', 'sync']);
-    grunt.registerTask('default', ['start', 'concat']);
+    watch: {
+      default: {
+        files: ['source/**'],
+        tasks: 'default'
+      },
+      options: {
+        spawn: false
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '// Apaxy 2 Server plugin: (Minified)\n\n'
+      },
+      build: {
+        src: 'builds/production/apache_plugin/apaxy_2.js',
+        dest: 'builds/production/apache_plugin/apaxy_2.min.js'
+      }
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-sync');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['init', 'concat', 'sync:extension', 'sync:plugin']);
+  grunt.registerTask('extension', ['init:extension', 'concat:extension', 'sync:extension']);
+  grunt.registerTask('plugin', ['init:plugin', 'concat:plugin', 'sync:plugin']);
+
+  grunt.registerTask('build', ['default', 'sync:build', 'uglify:build']);
+
 };
